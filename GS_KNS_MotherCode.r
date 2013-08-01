9c72f87af968b4893897340a052c5c3c527b57da
lauratb <laura.t.boyer@gmail.com>
2013-08-01 12:19:35 -0700

    rajout explication manuel et mother_code

--- GS_KNS_MotherCode.r
+++ GS_KNS_MotherCode.r
@@ -1,35 +1,66 @@
 # Analyses des données KNS (Ginger/Soproner)
 # Auteur: Laura Tremblay-Boyer, contact: l.boyer@fisheries.ubc.ca
-# Time-stamp: <2013-07-30 09:41:45 Laura>
+# Time-stamp: <2013-08-01 12:19:02 Laura>
+
+################################################################
+###### Définition des variables principales pour l'analyse #####
+## Cette partie du code est à MODIFIER MANUELLEMENT au besoin ##
+####### N'oubliez pas d'enregistrer tous les changements #######
+################################################################
+### 1. Filtre années
+### 2. Filtre espèces
+### 3. Sorties à produire (invertébrés, poissons, LIT)
+### 4. Définition de l'emplacement des fichiers
+################################################################
 
 # Utilisateur pour spécifier l'emplacement des dossiers
-usernow <- "Laura"
+usernow <- "Laura" # ou "Antoine", ou modifiez directement
+                   # l'emplacement des dossiers plus bas (section 4)
 
-# Mettre cette option à TRUE si vous voulez produire *toutes* les analyses, ou FALSE si vous
-# voulez ajuster certaines fonctions manuellement, ou tourner seulement certains des codes
-# tourner.tout <- FALSE # cette option n'est pas encore en place
+############################
+### 1. Filtre sur ANNEES ###
+############################
 
-# Définition des variables principales pour l'analyse:
-
-### Filtre sur ANNEES ### à completer
 filtre.annees <- 2006:2013 # indiquer quelles années à inclure dans l'analyse - tableau filtre généré automatiquement
                            # exemples de format:
                            # c(2006,2007,2009,2012), 2006:2012, seq(2006,2012,by=2)
 
-### Filtre sur ESPECES ###
+#############################
+### 2. Filtre sur ESPECES ###
+#############################
+
 filtre.sur.especes <- FALSE # pour inclure un filtre sur especes: filtre.sur.especes <- TRUE
 if(filtre.sur.especes) {
 
+    ## Modifiez les valeurs pour le filtre sur espèces ici!!!
+    ## Ces valeurs sont prises en compte seulement lorsque:
+    ## filtre.sur.especes = TRUE
+    ## Voir aussi la fonction def.filtre.especes() pour définir ces valeurs
+    ## dans la console, et la fonction voir.filtre.taxo() pour voir les valeurs
+    ## présentement enregistrées
+
+       taxoF.incl <- "inclure" # Inclure ou exclure le niveau taxonomique donnée?
+       taxoF.utaxo <- "Groupe" # Niveau taxonomique ciblé
+       taxoF.nom <- c("Crustaces","Mollusques","Echinodermes") # Nom des membres à inclure ou exclure
+
+       }else{
+    ## Valeurs automatiques (ne pas changer) qui indique à la fonction
+    ## filtreTaxo() de ne pas filtre les espèces
        taxoF.incl <- "inclure"
        taxoF.utaxo <- "Groupe"
-       taxoF.nom <- c("Crustaces","Mollusques","Echinodermes")
-       }else{
-         taxoF.incl <- "inclure"; taxoF.utaxo <- "Groupe"; taxoF.nom <- "Tous" }
+       taxoF.nom <- "Tous" }
 
-### Sorties à produire ###
+#############################
+### 3. Sorties à produire ###
+#############################
+
 sorties.INV <- FALSE # pour produire les sorties des invertébrés: sorties.INV <- TRUE
 sorties.LIT <- FALSE # pour produire les sorties pour le LIT: sorties.LIT <- TRUE
 sorties.poissons <- FALSE # pour produire les sorties des poissons: sorties.POISSONS <- TRUE
+
+#############################
+## 4. Définition DOSSIERS ###
+#############################
 
 # Dossier où les analyses sont faites, et qui contient le code R et les graphiques/tableaux produits
 # Attention de rajouter un "/" à la fin, selon le format "C:/.../dossier.R/"
@@ -39,10 +70,16 @@ dossier.R <- ifelse(usernow=="Antoine", "C:/dossier.R/","/Users/Laura/Projects/c
 dossier.DB <- ifelse(usernow=="Antoine","C:/dossier.DB/",
                      "/Users/Laura/Dropbox/KNS_GINGER-SOPRONER/DB_Dernieres_Versions/")
 
-options('stringsAsFactors'=FALSE) # option générale ôte les colonnes de type "factor" lors de l'import des données
+
 ######################################################
 ######################################################
+####### FIN DE LA PARTIE DU CODE MODIFIABLE ##########
+### (en théorie il n'y a rien à modifier plus bas) ###
+######################################################
+######################################################
+
 setwd(dossier.R) # défini le working directory de R
+options('stringsAsFactors'=FALSE) # option générale ôte les colonnes de type "factor" lors de l'import des données
 
 # Crée les dossiers Data/Tableaux/Graphiques dans dossier.R s'ils n'existent pas
 ndossiers <- c("Data","Tableaux","Graphiques")
